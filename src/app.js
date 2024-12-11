688f39c510d66a006c73d375434c94b60ed5df9f
EduardoGP <124112815+Eduardo7052311@users.noreply.github.com>
2024-12-11 22:17:15 +0000

    actualizacion random card

--- src/app.js
+++ src/app.js
@@ -6,6 +6,41 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function aleatorio(lista) {
+    let numAleatorio = Math.floor(Math.random() * lista.length);
+    return lista[numAleatorio];
+  }
+  let simbol = ["♦", "♥", "♠", "♣"];
+  let numero = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let simbolCarta = aleatorio(simbol);
+  let CartaArriba = document.querySelector("#cartaArriba");
+  CartaArriba.innerHTML = simbolCarta;
+
+  let numCarta = aleatorio(numero);
+  let elementNumCarta = document.querySelector("#numCarta");
+  elementNumCarta.innerHTML = numCarta;
+
+  let CartaAbajo = document.querySelector("#cartaAbajo");
+  CartaAbajo.innerHTML = simbolCarta;
+
+  CartaAbajo.style.transform = "rotate(180deg)";
+  if (simbolCarta == "♥" || simbolCarta == "♦") {
+    CartaAbajo.style.color = "red";
+    CartaArriba.style.color = "red";
+  }
 };
